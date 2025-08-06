3ba4c070af4d8c55527fe7612b32bb12acbe5039
analauracano <laura.fioratti@outlook.com>
2025-08-07 00:54:01 +0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+const startServer = async () => {
+    try {
+        await app.listen({ port: PORT}).then(() => { console.log( `Servidor rodando na porta ${PORT}`)});
+    } catch (err) {
+        console.error( err);
+    }
+}
+
+startServer()
